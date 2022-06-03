9479961af3a02b208ae89e74da8e97dea58488d3
Anil sahu <111anilsahu@gmail.com>
2022-06-03 04:03:54 -0700

    api post method completed

--- api/src/app.js
+++ api/src/app.js
@@ -8,14 +8,24 @@ const port = process.env.port || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
+//create post
+app.post("/api/student", async (req, res) => {
     try {
-        res.send("Connnection successful")
+        var mybody = req.body;
+        var name = mybody.name;
+        var mobile = mybody.mobile;
+        var email = mybody.email;
+        var branch = mybody.branch;
+        var fee = mybody.fee;
+        var sqlInsert = 'INSERT INTO student(name,mobile,email,branch,fee) VALUE (?,?,?,?,?)';
+        var sql = await pool.query(sqlInsert, [name, mobile, email, branch, fee],);
+        res.json(sql);
+        console.log(mybody);
     } catch (error) {
-        console.error(error);
+        console.error(error)
     }
+});
 
-})
 
 app.listen(port, () => {
     console.log("my server is running at", port)
