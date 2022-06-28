16ddef280ac1715834f2d8d9038e211f342d8ab8
Anil-sahu <68375571+Anil-sahu@users.noreply.github.com>
2022-06-28 02:56:44 -0700

    Update students.js

--- api/src/models/students.js
+++ api/src/models/students.js
@@ -1,14 +1,3 @@
 
 
 
-class Student {
-    constructor(name, mobile, email, branch, fee) {
-        this.name = name;
-        this.mobile = mobile;
-        this.email = email;
-        this.branch = branch;
-        this.fee = fee;
-    }
-}
-
-exports.module = Student;
